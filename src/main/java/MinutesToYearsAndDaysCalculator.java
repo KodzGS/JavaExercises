ee59481c299f86716abd491b1cff6fa2ba9d41b6
KodzGS <k.johnson@gymshark.com>
2023-10-04 08:16:24 +0100

    commit minutes to years calculator

---
+++ src/main/java/MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,42 @@
+public class MinutesToYearsAndDaysCalculator {
+
+    public static void main(String[] args) {
+        printYearsAndDays(3789456);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+
+        //If the parameter is less than 0, print text "Invalid Value".
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long hours = minutes / 60;
+            long days = hours / 24;
+            long years = days / 365;
+            long remainderDays = days % 365;
+            System.out.println(minutes + " min = " + years + " year(s) and " + remainderDays + " d ");
+
+        }
+    }
+}
+
+/*
+Exercise 10 - Minutes To Years and Days Calculator:
+Write a method printYearsAndDays with parameter of type long named minutes.
+The method should not return anything (void) and it needs to calculate the years and days from the minutes parameter.
+If the parameter is less than 0, print text "Invalid Value".
+Otherwise, if the parameter is valid then it needs to print a message in the format "XX min = YY y and ZZ d".
+XX represents the original value minutes.
+YY represents the calculated years.
+ZZ represents the calculated days.
+EXAMPLES OF INPUT/OUTPUT:
+printYearsAndDays(525600); → should print "525600 min = 1 y and 0 d"
+printYearsAndDays(1051200); → should print "1051200 min = 2 y and 0 d"
+printYearsAndDays(561600); → should print "561600 min = 1 y and 25 d"
+TIPS:
+Be extra careful about spaces in the printed message.
+Use the remainder operator
+1 hour = 60 minutes
+1 day = 24 hours
+1 year = 365 days
+ */
