b7497e4ba56c39293507657cede8172e3f42ae8c
KodzGS <k.johnson@gymshark.com>
2023-10-13 16:04:22 +0100

    Commit person task with updated changes (linespaces)

--- src/main/java/PersonTask.java
+++ src/main/java/PersonTask.java
@@ -1,6 +1,4 @@
 public class PersonTask {
-
-    // Write a class with the name Person. The class needs three fields (instance variables) with the names firstName, lastName of type String and age of type int.
     private String firstName;
     private String lastName;
     private int age;
@@ -12,6 +10,7 @@ public class PersonTask {
     public String getFirstName() {
         return this.firstName;
     }
+
     /**
      * Method named getLastName without any parameters, it needs to return the value of the lastName field.
      * @return method to return last name
@@ -27,6 +26,7 @@ public class PersonTask {
     public int getAge() {
         return this.age;
     }
+
     /**
      * Method named setFirstName with one parameter of type String, it needs to set the value of the firstName field.
      * @param firstName set the firstname string
@@ -34,6 +34,7 @@ public class PersonTask {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     /**
      * Method named setLastName with one parameter of type String, it needs to set the value of the lastName field.
      * @param lastName set the lastname string
@@ -41,6 +42,7 @@ public class PersonTask {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     /**
      * Method named setAge with one parameter of type int, it needs to set the value of the age field.
      * If the parameter is less than 0 or greater than 100, it needs to set the age field value to 0.
@@ -51,6 +53,7 @@ public class PersonTask {
             this.age = 0;
         } else this.age = age;
     }
+
     /**
      * Method named isTeen without any parameters, it needs to return true if the value of the age field is greater than 12 and less than 20, otherwise, return false.
      * @return age for teenagers
@@ -58,6 +61,7 @@ public class PersonTask {
     public boolean isTeen() {
         return age >= 12 && age <= 20;
     }
+
     /**
      * Method named getFullName without any parameters, it needs to return the full name of the person.
      * @return empty strings if one or other is empty
