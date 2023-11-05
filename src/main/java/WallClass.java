d93175e8aae471b96c517a794f1ad17bda5a3bc5
KodzGS <k.johnson@gymshark.com>
2023-11-05 08:10:00 +0000

    Commit wall exercise 17

---
+++ src/main/java/WallClass.java
@@ -0,0 +1,13 @@
+public class WallClass {
+
+    public static void main(String[] args) {
+        Wall wall = new Wall(7,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setWidth(7);
+        wall.setHeight(4);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
+
+}
