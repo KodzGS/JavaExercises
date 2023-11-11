5348c5f54e5de890b78a4c370e9e072c8510bb2c
KodzGS <k.johnson@gymshark.com>
2023-11-11 12:03:35 +0000

    Commit constructors changes

--- src/main/java/Constructors.java
+++ src/main/java/Constructors.java
@@ -1,6 +1,6 @@
 public class Constructors {
     public static void main(String[] args) {
-        Boxers boxers = new Boxers("Miguel Cotto", 43, 160.8);
+        Boxers boxers = new Boxers("Miguel Angel Cotto", 43, 160.8);
         System.out.println(boxers.getBoxerName() + " " + boxers.getAge() + " " + boxers.getWeight());
     }
 }
