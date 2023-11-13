12d9c2d372b5c81ebcb4b5a7e1244f5c1460de2f
KodzGS <k.johnson@gymshark.com>
2023-11-13 09:30:35 +0000

    Commit constructors change class name to Boxer.java

--- src/main/java/Boxer.java
+++ src/main/java/Boxer.java
@@ -1,4 +1,4 @@
-public class Boxers {
+public class Boxer {
 
     /**
      * Instantiate in Java means to call a constructor of a Class which creates an an instance or object,
@@ -10,7 +10,7 @@ public class Boxers {
     private final int age;
     private final double weight;
 
-    public Boxers(String boxerName, int age, double weight) {
+    public Boxer(String boxerName, int age, double weight) {
         this.boxerName = boxerName;
         this.age = age;
         this.weight = weight;
--- src/main/java/Constructors.java
+++ src/main/java/Constructors.java
@@ -1,6 +1,6 @@
 public class Constructors {
     public static void main(String[] args) {
-        Boxers boxers = new Boxers("Miguel Angel Cotto", 43, 160.8);
+        Boxer boxers = new Boxer("Miguel Angel Cotto", 43, 160.8);
         System.out.println(boxers.getBoxerName() + " " + boxers.getAge() + " " + boxers.getWeight());
     }
 }
