9dd080da1f9c2ce44352ba742428ab590ffae4a1
KodzGS <k.johnson@gymshark.com>
2023-11-25 13:40:24 +0000

    Commit constructors change class name to Boxer.java

---
+++ src/main/java/Contact.java
@@ -0,0 +1,34 @@
+
+public class Contact {
+
+    /**
+     * Two fields, both String, one called name and the other phoneNumber.
+     *   - A constructor that takes two Strings, and initialises name and phoneNumber.
+     *   - And Three methods, they are:
+     *      -  getName(), getter for name.
+     *      -  getPhoneNumber(), getter for phoneNumber.
+     *      -  createContact(), has two parameters of type String (the persons name and phone number) and returns an instance of Contact.
+     *         This is the only method that is static.
+     */
+
+        public final String name;
+        public final String phoneNumber;
+
+        public Contact(String name, String phoneNumber) {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+        public String getPhoneNumber(){
+            return this.phoneNumber;
+        }
+
+        public static Contact createContact(String name, String phoneNumber){
+            return new Contact(name,phoneNumber);
+
+        }
+    }
+
---
+++ src/main/java/MobileClass.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class MobileClass {
+
+    public static void main(String[] args) {
+
+        MobilePhone kodisPhone = new MobilePhone("112234455666");
+
+        Contact contact1 = Contact.createContact("Floyd Mayweather", "31415926");
+        Contact contact2 = Contact.createContact("Miguel Cotto", "16180339");
+        Contact contact3 = Contact.createContact("Pernell Whittaker", "11235813");
+        Contact contact4 = Contact.createContact("Katie Taylor", "11235813");
+        Contact contact5 = Contact.createContact("Anthony Joshua", "8899500005");
+        Contact contact6 = Contact.createContact("Oleksander Usyk", "8899500005");
+        Contact contact7 = Contact.createContact("Daniel Dubois", "8578993030");
+        Contact contact8 = Contact.createContact("Bud Crawford", "8899500005");
+        Contact contact9 = Contact.createContact("Andre Ward", "u7788488995");
+
+        kodisPhone.addNewContact(contact1);
+        kodisPhone.addNewContact(contact1);
+        kodisPhone.addNewContact(contact2);
+        kodisPhone.addNewContact(contact3);
+        kodisPhone.addNewContact(contact4);
+        kodisPhone.updateContact(contact5, contact6);
+        kodisPhone.addNewContact(contact8);
+        kodisPhone.removeContact(contact3);
+        kodisPhone.queryContact(contact5.getName());
+        kodisPhone.findContact(contact7.getName());
+        kodisPhone.removeContact(contact9);
+        kodisPhone.printContacts();
+
+
+    }
+}
+
+
+
+
