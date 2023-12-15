13a1695bbde532ce0f2e91f0e5ba4b705d6b6347
KodzGS <k.johnson@gymshark.com>
2023-12-15 16:36:26 +0000

    amended PR comments and reformatted project

--- src/main/java/Constructors.java
+++ src/main/java/Constructors.java
@@ -1,7 +1,8 @@
 public class Constructors {
-    public static void main(String[] args) {
-        Boxer boxers = new Boxer("Miguel Angel Cotto", 43, 160.8);
-        System.out.println(boxers.getBoxerName() + " " + boxers.getAge() + " " + boxers.getWeight());
-    }
+
+  public static void main(String[] args) {
+    Boxer boxers = new Boxer("Miguel Angel Cotto", 43, 160.8);
+    System.out.println(boxers.getBoxerName() + " " + boxers.getAge() + " " + boxers.getWeight());
+  }
 }
 
